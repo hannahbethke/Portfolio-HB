297416ebb0a403cab4f46a07bb4a4998cd84dfba
hannahbethke <hannahnichole@gmail.com>
2022-10-19 23:17:24 -0600

    tests card flip action and text animation

--- script.js
+++ script.js
@@ -1,41 +1,43 @@
 
-// Burger menus
-document.addEventListener('DOMContentLoaded', function() {
-    // open
-    const burger = document.querySelectorAll('.navbar-burger');
-    const menu = document.querySelectorAll('.navbar-menu');
 
-    if (burger.length && menu.length) {
-        for (var i = 0; i < burger.length; i++) {
-            burger[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
+function FrontOfCard() {
+    return (
+      `<div className="absolute inset-0 w-full h-full flex justify-center items-center bg-gray-900 transition-all duration-100 delay-200 z-20 hover:opacity-0">
+        FRONT OF CARD
+      </div>`
+    );
+  }
 
-    // close
-    const close = document.querySelectorAll('.navbar-close');
-    const backdrop = document.querySelectorAll('.navbar-backdrop');
+  function BackOfCard() {
+    return (
+      `<div className="absolute inset-0 w-full h-full flex justify-center items-center bg-black transition-all z-10 card-back">
+        BACK OF CARD
+      </div>`
+    );
+  }
 
-    if (close.length) {
-        for (var i = 0; i < close.length; i++) {
-            close[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
+ function FlipHover() {
+    return (
+      `<div className="relative w-96 h-60 rounded-2xl text-white overflow-hidden cursor-pointer transition-all duration-700 card">
+        <FrontOfCard />
+        <BackOfCard />
+      </div>`
+    );
+  }
 
-    if (backdrop.length) {
-        for (var i = 0; i < backdrop.length; i++) {
-            backdrop[i].addEventListener('click', function() {
-                for (var j = 0; j < menu.length; j++) {
-                    menu[j].classList.toggle('hidden');
-                }
-            });
-        }
-    }
-});
+  // Animate Lettering
+
+  let text = document.getElementById('text');
+  let newDom = '';
+  let animationDelay = 6;
+
+  for(let i = 0; i < text.innerText.length; i++) {
+      newDom += '<span class="char">' + (text.innerText[i] == ' ' ? '&nbsp;' : text.innerText[i])+ '</span>';
+  };
+
+  text.innerHTML = newDom;
+  let length = text.children.length;
+
+  for(let i = 0; i < length; i++) {
+      text.children[i].style['animation-delay'] = animationDelay * i + 'ms';
+  };
