d6bb0d3a1f4e4f06c7bb1ce794a8fad4dc686546
hannahbethke <hannahnichole@gmail.com>
2022-10-20 12:17:52 -0600

    wip: experimenting with flip cards for portfolio

--- script.js
+++ script.js
@@ -1,30 +1,11 @@
+const card = document.querySelector(".flip");
+const btns = document.querySelectorAll(".btn");
 
-
-function FrontOfCard() {
-    return (
-      `<div className="absolute inset-0 w-full h-full flex justify-center items-center bg-gray-900 transition-all duration-100 delay-200 z-20 hover:opacity-0">
-        FRONT OF CARD
-      </div>`
-    );
-  }
-
-  function BackOfCard() {
-    return (
-      `<div className="absolute inset-0 w-full h-full flex justify-center items-center bg-black transition-all z-10 card-back">
-        BACK OF CARD
-      </div>`
-    );
-  }
-
- function FlipHover() {
-    return (
-      `<div className="relative w-96 h-60 rounded-2xl text-white overflow-hidden cursor-pointer transition-all duration-700 card">
-        <FrontOfCard />
-        <BackOfCard />
-      </div>`
-    );
-  }
-
+btns.forEach((btn) => {
+  btn.addEventListener("click", ({target}) => {
+    target.closest('.flip').classList.toggle("isFlipped");
+  });
+});
   // Animate Lettering
 
   let text = document.getElementById('text');
