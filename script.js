49a4fb100b46794b7b81f106efa8094009cf69ee
hannahbethke <hannahnichole@gmail.com>
2022-11-16 11:43:06 -0700

    cleans code

--- script.js
+++ script.js
@@ -12,16 +12,19 @@ btns.forEach((btn) => {
 const mobileNavMenu = document.querySelector("#mobileNavMenu");
 const burgerIcon = document.querySelector("#burgerIcon");
 const closeIcon = document.querySelector("#closeIcon");
+const mainNav = document.querySelector("#mainNav");
 
 function mobileMenuOpen() {
   mobileNavMenu.style.display = "flex";
   mobileNavMenu.style.flexDirection = "column";
   burgerIcon.style.display = "none";
+  mainNav.style.background = "transparent";
 };
 
 function mobileMenuClose() {
   mobileNavMenu.style.display = "none";
   burgerIcon.style.display = "flex";
+  mainNav.style.background = "white";
 };
 
 burgerIcon.addEventListener("click", mobileMenuOpen);
